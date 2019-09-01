d4832d835809892e001e450a49ad97211d63f268
Alan <1060908200@qq.com>
2019-09-01 10:33:10 +0800

    修正了一个bug

--- a.java
+++ a.java
@@ -5,5 +5,9 @@ public void a(){
 	System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
 	System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
 	System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
+	System.out.println("fix some bug introduced by the previous commit d");
+	System.out.println("fix some bug introduced by the previous commit d");
+	System.out.println("fix some bug introduced by the previous commit d");
+	System.out.println("fix some bug introduced by the previous commit d");
 	
 }
